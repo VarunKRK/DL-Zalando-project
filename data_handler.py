8751a9e99349126a04b7fbe7a8221c403668a4b9
Varun Kota <vkr.k8055@gmail.com>
2022-04-09 11:30:57 +0200

    start

---
+++ data_handler.py
@@ -0,0 +1,23 @@
+from operator import index
+import pandas as pd
+import numpy as np
+
+import torch
+from sklearn.model_selection import train_test_split
+from torchvision import datasets, transforms
+
+
+def get_data():
+    # Define a transform to normalize the data
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5,), (0.5,))])
+
+    # Download and load the training data
+    trainset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+    # Download and load the test data
+    testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+    
+    return trainset, testset, trainloader, testloader
